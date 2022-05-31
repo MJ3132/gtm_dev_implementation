b907ed2203b4ad802e40e1d43e5d31c80bdae295
Mjarrin <martin_jarrin@hotmail.com>
2022-05-31 13:12:05 -0400

    SPA GH pages updates

--- src/index.js
+++ src/index.js
@@ -8,11 +8,10 @@ import reportWebVitals from './reportWebVitals';
 import { BrowserRouter as Router } from 'react-router-dom';
 
 
-const baseUrl = process.env.NODE_ENV === 'development' ? '' : '/gtm_dev_implementation';
 
 ReactDOM.render(
   <React.StrictMode>
-    <Router basename={baseUrl}> 
+    <Router basename='/gtm_dev_implementation'> 
     <App />
     </Router>
   </React.StrictMode>,
